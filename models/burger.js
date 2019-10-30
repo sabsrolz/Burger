ad9580e31ad2021cf5453a986298a85e29b6568c
sabsrolz <sabsrolz@umich.edu>
2019-10-30 11:32:47 -0500

    created index html for app

---
+++ models/burger.js
@@ -0,0 +1,8 @@
+const orm = require("../config/orm");
+
+orm.selectAll();
+//"SELECT * FROM ??"
+orm.insertOne();
+//`INSERT INTO ?? (??, ??) VALUES ("??", ??)`;
+orm.updateOne();
+//`UPDATE ?? SET ?? = "??" WHERE ?? = "??"`
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require("express");
+
+let app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+let PORT = process.env.PORT || 8080;
+
+//Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("./public"));
+
+//routes
+
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
