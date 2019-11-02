3557e6c1154e3c4abbda75de00502ba4c80bbe78
sabsrolz <sabsrolz@umich.edu>
2019-11-01 20:33:29 -0500

    added remote db connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
 // Set up our connection information
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Coquisolz18",
-  database: "burgers_db"
-});
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Coquisolz18",
+    database: "burgers_db"
+  });
+}
 
 // Connect to the database
 connection.connect(function(err) {
